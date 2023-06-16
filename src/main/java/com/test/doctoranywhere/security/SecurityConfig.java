50a45518aff904e48653e7a16a78dc4802e38571
Cassia <e0564059@u.nus.edu>
2023-06-16 11:19:26 +0800

    disabled csrf

--- src/main/java/com/test/doctoranywhere/security/SecurityConfig.java
+++ src/main/java/com/test/doctoranywhere/security/SecurityConfig.java
@@ -2,12 +2,17 @@ package com.test.doctoranywhere.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 import org.springframework.security.core.userdetails.User;
 
 @Configuration
@@ -15,21 +20,26 @@ import org.springframework.security.core.userdetails.User;
 public class SecurityConfig {
 
 	@Bean
-	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-		http
-			.authorizeHttpRequests((requests) -> requests
-				.requestMatchers("/").permitAll()
-				.anyRequest().authenticated()
-			)
-			.formLogin((form) -> form
-				.loginPage("/login")
-				.permitAll()
-                .defaultSuccessUrl("/", true)
-			)
-			.logout((logout) -> logout.permitAll());
+public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    http
+        .authorizeHttpRequests((requests) -> requests
+            .anyRequest().authenticated() // Allow all other requests without authentication
+        )
+        .formLogin((form) -> form
+            .loginPage("/login")
+            .permitAll()
+            .defaultSuccessUrl("/", true)
 
-		return http.build();
-	}
+        )
+        .logout((logout) -> logout.permitAll())
+        .sessionManagement((session) -> session // Configure session management
+            .sessionFixation().migrateSession() // Migrate session on authentication
+            .maximumSessions(1) // Allow only one session per user
+        )
+		.csrf().disable(); // Disable CSRF protection for simplicity;
+
+    return http.build();
+}
 
 	@Bean
 	public UserDetailsService userDetailsService() {
